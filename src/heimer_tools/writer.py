df525a53ac8677edc477fcddeb7831f72eb37d67
Romilly Cocking <romilly.cocking@gmail.com>
2022-05-24 13:21:36 +0100

    Refactored formatting.

--- src/heimer_tools/writer.py
+++ src/heimer_tools/writer.py
@@ -6,17 +6,21 @@ DOT_PREFIX = 'digraph {\n'
 DOT_SUFFIX = '}'
 
 
+def table_row(text: str, bold: bool = False):
+    if bool:
+        text = '<B>%s</B>' % text
+    return '<TR><TD>%s</TD></TR>' % text
+
+
 def filter_text(node_text: str):
     lines = node_text.split('\n')
     html = io.StringIO()
     html.write('<<TABLE border="0">')
     for (count, line) in enumerate(lines):
         if line.endswith('.png'):
-            html.write('<TR><TD><IMG SRC="%s"/></TD></TR>' % line)
-        elif count == 0:
-            html.write('<TR><TD><B>%s</B></TD></TR>' % line)
+            html.write(table_row('<IMG SRC="%s"/>' % line))
         else:
-            html.write('<TR><TD>%s</TD></TR>' % line)
+            html.write(table_row(line, bold=count == 0))
     html.write('</TABLE>>')
     result = html.getvalue()
     html.close()
@@ -29,6 +33,7 @@ def write_contents(map: HeimerMap, r: io.StringIO):
         r.write('    %d [label = %s];\n' % (node.idx, text))
     for link in map.links:
         r.write('    %d -> %d;\n' % (link.start, link.end))
+
 
 def write_dot(map: HeimerMap) -> str:
     r = io.StringIO()
